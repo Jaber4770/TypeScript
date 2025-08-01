c45a6a2724747f15dc5335978fd6c793eff5ac8f
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-08-01 08:44:57 +0200

    first commit

---
+++ first.js
@@ -0,0 +1,5 @@
+console.log('Hello World!');
+var country = "Bangladesh";
+console.log(country);
+var func = function () { return console.log("arrow func"); };
+console.log(func());
--- first.ts
+++ first.ts
@@ -1,1 +1,7 @@
 console.log('Hello World!');
+
+const country = "Bangladesh";
+console.log(country);
+
+const func = () => console.log("arrow func");
+console.log(func());
